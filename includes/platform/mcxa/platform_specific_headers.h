88289bff80fb7ce25d609ca566894bbf851cf57b
Vit Solcik <vit.solcik@nxp.com>
2026-02-18 13:55:04 +0100

    [ZEP-5659][SGI_PKC][ZEP] Add Zephyr support for SGI PKC lib
    Add cmakes and kconfig for SGI PKC CLNS enablement
    within Zephyr OS.
    
    Signed-off-by: Vit Solcik <vit.solcik@nxp.com>

--- includes/platform/mcxa/platform_specific_headers.h
+++ includes/platform/mcxa/platform_specific_headers.h
@@ -1,15 +1,16 @@
- /*--------------------------------------------------------------------------*/
- /* Copyright 2021-2026 NXP                                                  */
- /*                                                                          */
- /* NXP Confidential and Proprietary. This software is owned or controlled   */
- /* by NXP and may only be used strictly in accordance with the applicable   */
- /* license terms.  By expressly accepting such terms or by downloading,     */
- /* installing, activating and/or otherwise using the software, you are      */
- /* agreeing that you have read, and that you agree to comply with and are   */
- /* bound by, such license terms.  If you do not agree to be bound by the    */
- /* applicable license terms, then you may not retain, install, activate or  */
- /* otherwise use the software.                                              */
- /*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+/* Copyright 2021-2026 NXP                                                  */
+/*                                                                          */
+/* NXP Proprietary. This software is owned or controlled by NXP and may     */
+/* only be used strictly in accordance with the applicable license terms.   */
+/* By expressly accepting such terms or by downloading, installing,         */
+/* activating and/or otherwise using the software, you are agreeing that    */
+/* you have read, and that you agree to comply with and are bound by, such  */
+/* license terms. If you do not agree to be bound by the applicable license */
+/* terms, then you may not retain, install, activate or otherwise use the   */
+/* software.                                                                */
+/*--------------------------------------------------------------------------*/
+
  
  #ifndef PLATFORM_SPECIFIC_HEADERS_H_ 
  #define PLATFORM_SPECIFIC_HEADERS_H_ 
@@ -19,61 +20,8 @@
  #include "mcuxCsslAnalysis.h"
  
  MCUX_CSSL_ANALYSIS_START_PATTERN_EXTERNAL_HEADER()
+ #include "fsl_device_registers.h"
  #include "ip_platform.h" 
- #include "MCXA366.h" 
- #include "MCXA366_COMMON.h" 
- #include "MCXA366_features.h" 
- #include "system_MCXA366.h" 
- #include "PERI_ADC.h" 
- #include "PERI_AOI.h" 
- #include "PERI_CAN.h" 
- #include "PERI_CDOG.h" 
- #include "PERI_CMC.h" 
- #include "PERI_CRC.h" 
- #include "PERI_CTIMER.h" 
- #include "PERI_DEBUGMAILBOX.h" 
- #include "PERI_DIGTMP.h" 
- #include "PERI_DMA.h" 
- #include "PERI_EIM.h" 
- #include "PERI_EQDC.h" 
- #include "PERI_ERM.h" 
- #include "PERI_FLEXIO.h" 
- #include "PERI_FMC.h" 
- #include "PERI_FMU.h" 
- #include "PERI_FREQME.h" 
- #include "PERI_GLIKEY.h" 
- #include "PERI_GPIO.h" 
- #include "PERI_I3C.h" 
- #include "PERI_INPUTMUX.h" 
- #include "PERI_LCD.h" 
- #include "PERI_LPCMP.h" 
- #include "PERI_LPDAC.h" 
- #include "PERI_LPI2C.h" 
- #include "PERI_LPSPI.h" 
- #include "PERI_LPTMR.h" 
- #include "PERI_LPUART.h" 
- #include "PERI_MAU.h" 
- #include "PERI_MRCC.h" 
- #include "PERI_OPAMP.h" 
- #include "PERI_OSTIMER.h" 
- #include "PERI_PKC.h" 
- #include "PERI_PORT.h" 
- #include "PERI_PWM.h" 
- #include "PERI_RTC.h" 
- #include "PERI_SCG.h" 
- #include "PERI_SGI.h" 
- #include "PERI_SMARTDMA.h" 
- #include "PERI_SPC.h" 
- #include "PERI_SYSCON.h" 
- #include "PERI_TRDC.h" 
- #include "PERI_TRNG.h" 
- #include "PERI_UDF.h" 
- #include "PERI_USB.h" 
- #include "PERI_UTICK.h" 
- #include "PERI_VBAT.h" 
- #include "PERI_WAKETIMER.h" 
- #include "PERI_WUU.h" 
- #include "PERI_WWDT.h" 
  MCUX_CSSL_ANALYSIS_STOP_PATTERN_EXTERNAL_HEADER()
  
  #endif /*PLATFORM_SPECIFIC_HEADERS_H_*/ 
